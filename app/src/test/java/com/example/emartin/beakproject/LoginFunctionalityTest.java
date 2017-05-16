1befa68a5ab476c10b577e342051f4a7aaa4ccda
edsmartin7 <edsmartin7@gmail.com>
2017-05-15 23:08:51 -0400

    test start

---
+++ app/src/test/java/com/example/emartin/beakproject/LoginFunctionalityTest.java
@@ -0,0 +1,19 @@
+package com.example.emartin.beakproject;
+
+import org.junit.Test;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+//https://developer.android.com/training/testing/unit-testing/local-unit-tests.html
+//https://developer.android.com/training/testing/start/index.html
+
+public class LoginFunctionalityTest {
+
+    @Test
+    public void login_CorrectLogin_ReturnsTrue(){
+
+    }
+
+
+}
